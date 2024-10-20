dc136c4c782b26805d64a1468bacf7f29ff5688e
Allpez <alexlop1987@gmail.com>
2024-10-19 20:07:48 -0500

    creacion de la aplicacion de express basica, dependencias y variable de entorno

--- README.md
+++ README.md
@@ -3,5 +3,8 @@
 
 *** Para poder correr el proyecto debe ejecutar los siguientes comandos: ***
 
- - npm install express
+  - npm install express
+  - npm install --save-dev nodemon
+  - npm install dotenv
+
 
--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-import express from "express"; // Importamos express de la carpeta "node_modules", este archivo va a levantar la aplicacion
+import express from "express"; // Importamos express de la carpeta "node_modules", este archivo va a levantar la aplicación
+import "dotenv/config.js" // Nos poermite acceder a las variables de entorno
+
+const server = express(); //creamos la variable server donde se ejecutara express
+
+const PORT = process.env.PORT || 8080 //variable PORT donde se almacenara la ruta al servidor (variable de entorno)
+
+const ready = () => {
+    console.log("server ready in port: " +PORT); //funcion parea retornar el estado en la terminal
+}
+
+server.listen(PORT,ready) //utilizamos la propiedad "listen" para escuchar el puerto y levantar el servidor
+
+console.log(process.env.PORT); //para ver las variables de entorno
