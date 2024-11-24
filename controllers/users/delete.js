293b52b9d25cf19d6ae6f74a2d68dd23d157dd34
Allpez <alexlop1987@gmail.com>
2024-11-24 03:49:20 -0500

    guardado

--- controllers/users/delete.js
+++ controllers/users/delete.js
@@ -3,7 +3,7 @@ import User from "../../models/User.js"
 let deleteOne = async (req, res, next) => {
     try {
         let deleteUser = await User.deleteOne({ //El metodo que utilizamos para eliminar 1
-            name: req.body.name //Condiciones para buscar el que compla la condicion y eliminarlo
+            firstname: req.body.firstname //Condiciones para buscar el que compla la condicion y eliminarlo
             // Este método busca el primer documento que cumple ciertas condiciones y lo elimina. Devuelve una propiedad deletedCount con la cantidad de documentos eliminados (aunque en este caso sea solo uno).
         })
         return res.status(200).json({
@@ -19,7 +19,7 @@ const deleteManyUsers = {
         try {
             // Elimina todos los documentos que coincidan con el `admin_id` proporcionado
             let destroyed = await User.deleteMany(
-                { name: req.body.name } // Condiciones para la búsqueda y eliminación
+                { firstname: req.body.firstname } // Condiciones para la búsqueda y eliminación
             )
             return res.status(200).json({ response: destroyed }) // Retorna la respuesta con `deletedCount`
         } catch (error) {
